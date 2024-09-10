61c713f8bf81ff9e16f6b570d888857fe7683017
Crisandry Javier <crisandryj@gmail.com>
2024-09-10 15:47:46 -0400

    Feat: Add components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { Link } from "react-router-dom";
 
 const App = () => {
   return (
@@ -11,7 +12,7 @@ const App = () => {
       <nav>
         <ul>
           <li>
-            <a href="profile">Profile page</a>
+            <Link to="profile">Profile page</Link>
           </li>
         </ul>
       </nav>
--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,8 +1,11 @@
+import { Outlet } from "react-router-dom";
+
 const Profile = () => {
   return (
     <div>
       <h1>Hello from profile page!</h1>
       <p>So, how are you?</p>
+      <Outlet />
     </div>
   );
 };
