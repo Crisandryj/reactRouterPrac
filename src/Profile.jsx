6802618be973f5456799bfa21fadd8e524f3cd4f
Crisandry Javier <crisandryj@gmail.com>
2024-09-11 16:40:43 -0400

    Feat: Add error page and profile'

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,11 +1,24 @@
-import { Outlet } from "react-router-dom";
+import { useParams } from "react-router-dom";
+import DefaultProfile from "./DefaultProfile";
+import Spinach from "./Spinach";
+import Popeye from "./Popeye";
 
 const Profile = () => {
+  const { name } = useParams();
+
   return (
     <div>
       <h1>Hello from profile page!</h1>
       <p>So, how are you?</p>
-      <Outlet />
+      <hr />
+      <h2>The profile visited is here:</h2>
+      {name === "popeye" ? (
+        <Popeye />
+      ) : name === "spinach" ? (
+        <Spinach />
+      ) : (
+        <DefaultProfile />
+      )}
     </div>
   );
 };
