cb3d65a15cd135aa9edf9afc30d112c69450dfc8
Crisandry Javier <crisandryj@gmail.com>
2024-09-12 16:25:21 -0400

    Feat: Add links to components

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -2,6 +2,7 @@ import { useParams } from "react-router-dom";
 import DefaultProfile from "./DefaultProfile";
 import Spinach from "./Spinach";
 import Popeye from "./Popeye";
+import { Link } from "react-router-dom";
 
 const Profile = () => {
   const { name } = useParams();
@@ -12,6 +13,9 @@ const Profile = () => {
       <p>So, how are you?</p>
       <hr />
       <h2>The profile visited is here:</h2>
+      <Link to="/profile/popeye">Popeye</Link>
+      <br />
+      <Link to="/profile/spinach">Spinach</Link>
       {name === "popeye" ? (
         <Popeye />
       ) : name === "spinach" ? (
